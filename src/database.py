993406ec6fc8f2ad0247dd824ee8c32e0c38523e
Arnav Sharma <arnav.sharma.1127@gmail.com>
2026-06-26 10:22:30 +0530

    Implement database class

---
+++ src/database.py
@@ -0,0 +1,58 @@
+"""
+database.py
+
+Responsible for storing and loading discovered devices.
+"""
+from pathlib import Path
+import sqlite3
+
+from device import Device
+DATABASE_PATH = Path(__file__).parent.parent / "languard.db"
+class Database:
+
+    def __init__(self):
+
+        self.connection = sqlite3.connect(DATABASE_PATH)
+
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Devices(
+            MAC TEXT PRIMARY KEY,
+            IP TEXT
+        )
+        """)
+
+        self.connection.commit()
+
+
+    def Save(self, devices: list[Device]):
+
+        self.cursor.execute("DELETE FROM Devices")
+
+        for device in devices:
+
+            self.cursor.execute(
+                """
+                INSERT INTO Devices(MAC, IP)
+                VALUES(?, ?)
+                """,
+                (device.MAC, device.IP)
+            )
+
+        self.connection.commit()
+
+
+    def Load(self) -> list[Device]:
+
+        devices = []
+
+        self.cursor.execute("SELECT IP, MAC FROM Devices")
+
+        rows = self.cursor.fetchall()
+
+        for ip, mac in rows:
+
+            devices.append(Device(ip, mac))
+
+        return devices
